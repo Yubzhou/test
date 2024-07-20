aa2156a797d8546543062568396e091de5324417
Yubzhou <yubzhou@qq.com>
2024-07-20 21:35:34 +0800

    first commit

---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import { BACKEND_PORT } from './config.js';
+// A helper you may want to use when uploading new images to the server.
+import { fileToDataUrl, renderLogin, renderRegister } from './helpers.js';
+
+renderLogin();
+
+
